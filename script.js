73f5184c69df3545511c3fa08df64092a906f307
robertomorano <roberto.morano@iesnervion.es>
2024-11-21 19:28:06 +0100

    finished

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const container = document.querySelector("#container");
-function createDivSquare(){
-
-        for(let i=0 ;i<(16*16);i++){
+let size=16;
+const button= document.querySelector("#btn");
+function createDivSquare(size){
+    
+        for(let i=0 ;i<(size*size);i++){
     const content = document.createElement("div");
     content.classList.add("grid-square");
     container.appendChild(content);
     }
- }
-createDivSquare();
+ }  
+createDivSquare(size);
+button.addEventListener("click", () => {
+    size=prompt("Dame un tamaño");  
+    createDivSquare(size);
+});
